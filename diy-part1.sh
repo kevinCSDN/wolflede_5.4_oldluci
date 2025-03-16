e75d5754589942f2f970c477bdf50b59a7c5c1c7
kevinCSDN <81788909+kevinCSDN@users.noreply.github.com>
2025-03-16 09:56:15 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,7 +16,7 @@
 #rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
 
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '2i src-git small https://github.com/kevinCSDN/small' feeds.conf.default
+sed -i '2i src-git small https://github.com/kevinCSDN/small_2024' feeds.conf.default
 
 注释掉新版luci
 sed -i 's|^#src-git luci https://github.com/coolsnowwolf/luci$|src-git luci https://github.com/coolsnowwolf/luci|' feeds.conf.default
